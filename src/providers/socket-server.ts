66ea896f639d0d0d0dd3dc2956f6a46d978bad87
皇冠鸟 <xxhgnxx@qq.com>
2017-03-01 10:17:05 +0800

    123

--- src/providers/socket-server.ts
+++ src/providers/socket-server.ts
@@ -33,9 +33,9 @@ export class SocketService {
 
         } else {
 
-            // this.socket = io.connect('192.168.1.14:81', { reconnection: false });
-            // this.socket = io.connect('127.0.0.1:81', {reconnection: false}); this.socket
-            this.socket = io.connect('hk.airir.com:81', { reconnection: false });
+            // this.socket = io.connect('http://192.168.1.14:81', { reconnection: false });
+            // this.socket = io.connect('http://127.0.0.1:81', {reconnection: false}); this.socket
+            this.socket = io.connect('http://hk.airir.com:81', { reconnection: false });
             return new Promise(resolve => {
                 let tmptimer = setTimeout(() => {
                     console.log(Date().toString().slice(15, 25), '连接服务器', '失败,重试');
